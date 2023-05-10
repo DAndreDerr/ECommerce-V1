07fd956d181dbfe1a8136db90a4c7c2a31365b05
DAndreDerr <dandre020727@gmail.com>
2023-05-09 23:30:59 -0400

    updated

--- app.js
+++ app.js
@@ -1,58 +1,31 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-// Fetch the JSON file
 fetch('products.json')
   .then(response => response.json())
   .then(data => {
-    // Loop through the products and create HTML elements
-    data.products.forEach(product => {
-      const productContainer = document.createElement('div');
-      const productImage = document.createElement('img');
-      const productTitle = document.createElement('h2');
-      const productDescription = document.createElement('p');
-      const productPrice = document.createElement('span');
+    const productsContainer = document.getElementById('products-container');
+    const products = data.products;
+    
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.classList.add('product');
       
-      // Set the content and attributes for each element
-      productImage.src = product.image;
-      productTitle.textContent = product.title;
-      productDescription.textContent = product.description;
-      productPrice.textContent = `$${product.price.toFixed(2)}`;
+      const imageElement = document.createElement('img');
+      imageElement.src = product.image;
+      productElement.appendChild(imageElement);
       
-      // Append the elements to the container
-      productContainer.appendChild(productImage);
-      productContainer.appendChild(productTitle);
-      productContainer.appendChild(productDescription);
-      productContainer.appendChild(productPrice);
+      const titleElement = document.createElement('h2');
+      titleElement.textContent = product.title;
+      productElement.appendChild(titleElement);
       
-      // Append the container to the page
-      document.getElementById('product-list').appendChild(productContainer);
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = product.description;
+      productElement.appendChild(descriptionElement);
+      
+      const priceElement = document.createElement('p');
+      priceElement.textContent = `$${product.price}`;
+      productElement.appendChild(priceElement);
+      
+      productsContainer.appendChild(productElement);
     });
   })
   .catch(error => console.error(error));
+
