bb86aa1b59a9e22209c3e1f1dc582e49b2863b2f
DAndreDerr <dandre020727@gmail.com>
2023-05-11 10:47:29 -0400

    added notes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+// The code below is making an HTTP request to the server to fetch my json file using the `fetch` function
 fetch('products1.json')
+// Once the server response is received we use the "response.json()" method to parse the data into JS file
   .then(response => response.json())
   .then(data => {
+    // below I created variables, inside those I use .document.getElementID() 
+    // and inside of that I linked the container I have on my html using the ID 
     const productsContainer = document.getElementById('products-container');
     const products = data.products;
-    
+    // Next I use the .forEach method to run through each product in the JSON array
     products.forEach(product => {
+    // For each product it creates a new the code below creates a div container with the "product" class 
       const productElement = document.createElement('div');
       productElement.classList.add('product');
-      
+    //  Below is the code that creates the img, h2, p(description), and p(price)
+    // after it creates the elements it appends them to the product div
       const imageElement = document.createElement('img');
       imageElement.src = product.image;
       productElement.appendChild(imageElement);
@@ -24,9 +30,11 @@ fetch('products1.json')
       priceElement.textContent = `$${product.price}`;
       productElement.appendChild(priceElement);
       
+    //   once all the elements are in the div I append the div we just created to the original "product" container (this pushes it to html)
       productsContainer.appendChild(productElement);
     });
   })
+//   below is code that will display error in the console if my code fails to fetch data from my JSON
   .catch(error => console.error(error));
 
 fetch('products2.json')
--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// here we create the index variable that keeps track of which image is being displayed
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -11,18 +12,39 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+//The function below displays the appropriate image and updates the dots
+// the n parameter is the placeholder for the index of what slide is being displayed
+
 function showSlides(n) {
+  // here we define the index for the for loops
   let i;
+  // here we get the grab all the elements with the classes "mySlides" and "dot"
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  // the first two ifs check if n is greater than the number of slides
+  // If so it sets the "slideIndex" to 1, and if n is less than 1 it sets
+  // the "slideIndex to the last slide"
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  // This for loop, loops through the slides and sets the display to "none"
+  // This hides the slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  // this loop goes through and removes the active class to the dots depending on current slide
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // this section sets whatever the current slide is to block display
   slides[slideIndex-1].style.display = "block";
+  // this section adds the active class to the dot that relates to current slide
   dots[slideIndex-1].className += " active";
 }
