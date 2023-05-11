7b4c82b34e4a4c0c2e1ad30c778e5e0898b776ac
DAndreDerr <dandre020727@gmail.com>
2023-05-10 22:50:39 -0400

    AAAAAhHHH

--- app.js
+++ app.js
@@ -1,7 +1,69 @@
-fetch('products.json')
+fetch('products1.json')
   .then(response => response.json())
   .then(data => {
     const productsContainer = document.getElementById('products-container');
+    const products = data.products;
+    
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.classList.add('product');
+      
+      const imageElement = document.createElement('img');
+      imageElement.src = product.image;
+      productElement.appendChild(imageElement);
+      
+      const titleElement = document.createElement('h2');
+      titleElement.textContent = product.title;
+      productElement.appendChild(titleElement);
+      
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = product.description;
+      productElement.appendChild(descriptionElement);
+      
+      const priceElement = document.createElement('p');
+      priceElement.textContent = `$${product.price}`;
+      productElement.appendChild(priceElement);
+      
+      productsContainer.appendChild(productElement);
+    });
+  })
+  .catch(error => console.error(error));
+
+fetch('products2.json')
+  .then(response => response.json())
+  .then(data => {
+    const productsContainer = document.getElementById('products-container2');
+    const products = data.products;
+    
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.classList.add('product');
+      
+      const imageElement = document.createElement('img');
+      imageElement.src = product.image;
+      productElement.appendChild(imageElement);
+      
+      const titleElement = document.createElement('h2');
+      titleElement.textContent = product.title;
+      productElement.appendChild(titleElement);
+      
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = product.description;
+      productElement.appendChild(descriptionElement);
+      
+      const priceElement = document.createElement('p');
+      priceElement.textContent = `$${product.price}`;
+      productElement.appendChild(priceElement);
+      
+      productsContainer.appendChild(productElement);
+    });
+  })
+  .catch(error => console.error(error));
+
+fetch('products3.json')
+  .then(response => response.json())
+  .then(data => {
+    const productsContainer = document.getElementById('products-container3');
     const products = data.products;
     
     products.forEach(product => {
---
+++ community.js
@@ -0,0 +1,26 @@
+var i = 0;
+var images = [];
+var time = 2000;
+
+images[0] = 'catches/catch1.webp';
+images[1] = 'catches/catch2.webp';
+images[2] = 'catches/catch3.webp';
+images[3] = 'catches/catch4.webp';
+images[4] = 'catches/catch5.webp';
+images[5] = 'catches/catch6.webp';
+images[6] = 'catches/catch7.webp';
+images[7] = 'catches/catch8.webp';
+
+function changeImg(){
+    document.slide.src = images[i]
+
+    if(i < images.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time)
+}
+
+window.onload = changeImg;
